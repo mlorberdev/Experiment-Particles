742fb175cba95faa665f39feaa77d1c044c520e9
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-01-11 06:55:07 -0500

    v1.2.0

--- no2.js
+++ no2.js
@@ -1,201 +1,40 @@
-// import { palettes } from "./palettes.js";
+import { particles, ctx, w, h, stop, dropping } from "./script.js";
 
 export function no2() {
-  // let hues = palettes[Math.floor(Math.random() * palettes.length)];
-  // INITIALIZE CANVAS
 
-  let canvas = document.getElementById("c");
-  let ctx = canvas.getContext("2d");
+  for (let i = 0; i < particles.length; i++) {
 
-  // canvas dimensions
+    let p = particles[i];
 
-  let w;
-  let h;
+    ctx.beginPath();
 
-  // canvas pixel ratio
+    // particle gradient
 
-  setupCanvas();
-  function setupCanvas() {
-    w = innerWidth;
-    h = innerHeight;
-    let ratio = window.devicePixelRatio || 1;
-    let bound = canvas.getBoundingClientRect();
-    canvas.width = bound.width * ratio;
-    canvas.height = bound.height * ratio;
-    ctx.scale(ratio, ratio);
+    let gradient = ctx.createRadialGradient(p.x, p.y, 0, p.x, p.y, p.radius);
+
+    gradient.addColorStop(.1, p.invert);
+    gradient.addColorStop(0.3, p.color);
+    gradient.addColorStop(.9, "transparent");
+
+    ctx.shadowColor = "#00000033";
+    ctx.shadowOffsetX = p.radius * 0.8;
+    ctx.shadowOffsetY = p.radius * 0.8;
+    ctx.fillStyle = gradient;
+    ctx.arc(p.x, p.y, p.radius, Math.PI * 2, false);
+    ctx.fill();
+
+    // add velocity illusion
+
+    p.x += p.vx * Math.cos(Math.random() * Math.PI * 2);
+    p.y += p.vy * Math.sin(Math.random() * Math.PI * 2);
+
+    // bounce off the sides
+
+    if (p.x < 0) { p.vx = -p.vx }
+    if (p.y < 0) { p.vy = -p.vy; }
+    if (p.x > w) { p.vx = -p.vx; }
+    if (p.y > h) { p.vy = -p.vy; }
+
   }
-
-  // PARTICLE CONSTRUCTOR
-
-  class particle {
-    constructor() {
-
-      // position
-
-      this.x = parseInt(Math.random() * w);
-      this.y = parseInt(Math.random() * h);
-
-      // color
-
-      let r = parseInt(Math.random() * 255);
-      let g = parseInt(Math.random() * 255);
-      let b = parseInt(Math.random() * 255);
-      this.color = `rgba(${r},${g},${b},0.5)`;
-      this.invert = `rgba(${255 - r},${255 - g},${255 - g},0.5)`
-
-      // size
-
-      this.radius = parseInt(Math.random() * 20) + 15;
-
-      // velocity
-
-      this.vx = Math.random().toFixed(2) * 5 + 3;
-      this.vy = Math.random().toFixed(2) * 5 + 3;
-    }
-  }
-
-  // PARTICLE CONTAINER
-
-  let step, n = 50, particles = [];
-
-  // add ele to array
-
-  for (let i = 0; i < n; i++) particles.push(new particle());
-
-  // CANVAS DRAW (ANIMATION)
-
-  let F = 0;
-
-  function draw() {
-
-    // clear canvas
-
-    if (F === 10) { F = 0; }
-    else { F++; }
-
-    // draw from particle array
-
-    for (let i = 0; i < n; i++) {
-
-      let p = particles[i];
-
-      ctx.beginPath();
-
-      // particle gradient
-
-      let gradient = ctx.createRadialGradient(p.x, p.y, 0, p.x, p.y, p.radius);
-
-      gradient.addColorStop(.1, p.invert);
-      gradient.addColorStop(0.3, p.color);
-      gradient.addColorStop(.9, "transparent");
-
-      ctx.shadowColor = "#00000033";
-      ctx.shadowOffsetX = p.radius * 0.8;
-      ctx.shadowOffsetY = p.radius * 0.8;
-      ctx.fillStyle = gradient;
-      ctx.arc(p.x, p.y, p.radius, Math.PI * 2, false);
-      ctx.fill();
-
-      // add velocity illusion
-
-      p.x += p.vx * Math.cos(Math.random() * Math.PI * 2);
-      p.y += p.vy * Math.sin(Math.random() * Math.PI * 2);
-
-      // bounce off the sides
-
-      if (p.x < 0) { p.vx = -p.vx }
-      if (p.y < 0) { p.vy = -p.vy; }
-      if (p.x > w) { p.vx = -p.vx; }
-      if (p.y > h) { p.vy = -p.vy; }
-
-    }
-    step = window.requestAnimationFrame(draw);
-  }
-
-  // ON-OFF SWITCH
-
-  let dropping = false;
-
-  document.getElementById("balldrop").addEventListener("click", function () {
-
-    if (dropping) {
-      dropping = false;
-      window.cancelAnimationFrame(step);
-      ctx.clearRect(0, 0, w, h);
-    }
-    else {
-      dropping = true;
-      window.requestAnimationFrame(draw);
-    }
-  });
-
-  // WINDOW RESIZE DEBOUNCE
-
-  const debounce = (func, delay = 250) => {
-
-    let debounceTimer;
-    return function () {
-      const context = this;
-      const args = arguments;
-      clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(() => func.apply(context, args), delay);
-    }
-  }
-  window.addEventListener("resize", () => {
-    window.cancelAnimationFrame(step);
-    window.cancelAnimationFrame(draw);
-    ctx.clearRect(0, 0, w, h);
-    setupCanvas();
-  });
-  window.addEventListener("resize", debounce(function () {
-
-    particles = [];
-    for (let i = 0; i < n; i++) {
-      particles.push(new particle());
-    }
-
-    if (dropping === true) window.requestAnimationFrame(draw);
-
-  }));
-
-  // PAUSE
-
-  document.getElementById("pause").addEventListener("click", function () {
-    if (dropping === true) {
-      dropping = false;
-      window.cancelAnimationFrame(step);
-      window.cancelAnimationFrame(draw);
-    } else {
-      dropping = true;
-      // window.requestAnimationFrame(step);
-      window.requestAnimationFrame(draw);
-    }
-  });
-
-  // RELOAD
-
-  document.getElementById("reload").addEventListener("click", () => {
-    dropping = false;
-    window.cancelAnimationFrame(step);
-    window.cancelAnimationFrame(draw);
-    ctx.clearRect(0, 0, w, h);
-    particles = [];
-    for (let i = 0; i < n; i++) particles.push(new particle());
-    dropping = true;
-    window.requestAnimationFrame(draw);
-  });
-
-  // DOWNLOAD
-
-  document.getElementById("download").addEventListener("click", function () {
-    let c = document.getElementById("c");
-    let type = "jpg";
-    let ele = document.createElement("a");
-    document.body.appendChild(ele);
-    ele.download = `mlorber_particles.${type}`;
-    ele.href = c.toDataURL(`image/${type};base64`);
-    ele.click();
-    ele.remove();
-
-  });
+  particles.length > 0 && dropping === true ? window.requestAnimationFrame(no2) : window.cancelAnimationFrame(no2);
 }
--- script.js
+++ script.js
@@ -1,19 +1,102 @@
 import { no1 } from "./no1.js";
 import { no2 } from "./no2.js";
-import { no3 } from "./no3.js";
+// import { no3 } from "./no3.js";
 
-!(function () {
+export let particles = [];
+export let canvas = document.getElementById("c");
+export let ctx = canvas.getContext("2d");
+export let w = innerWidth;
+export let h = innerHeight;
+export let stop = false;
+export let dropping = false;
 
-  // RESPOND TO USER SELECTION BY RUNNING SELECTED PROGRAM
-  document.getElementById("balldrop").addEventListener("change", function () {
-    switch (this.value) {
-      case "no1": no1(); break;
-      case "no2": no2(); break;
-      case "no3": no3(); break;
+export function main() {
+
+  // VARIABLES
+  let step; // will be step = requestAnimationFrame([app])
+  let ratio = window.devicePixelRatio || 1;
+  let bound = canvas.getBoundingClientRect(); // defined in CSS
+  canvas.width = bound.width * ratio;
+  canvas.height = bound.height * ratio;
+  ctx.scale(ratio, ratio); // now drawing context should be pixel-perfect!
+  const rn = (z) => { return parseInt(Math.random() * z); } // returns a random number 0 - z
+
+  // PARTICLE CONSTRUCTOR exports particle array with each p's position, color, invert color (grad stop ~ .1), size and velocity
+  class Particle {
+    constructor() {
+
+      // position
+      this.x = rn(w);
+      this.y = rn(h);
+
+      // color
+      let r = rn(255);
+      let g = rn(255);
+      let b = rn(255);
+      let a = 
+      this.color = `rgba(${r},${g},${b},0.5)`;
+      this.invert = `rgba(${255 - r},${255 - g},${255 - g},0.5)`
+
+      // size
+      this.radius = rn(20) + 15;
+
+      // velocity
+      this.vx = rn(2) + 1;
+      this.vy = rn(2) + 1;
+    }
+  }
+
+  // LOAD ARRAY
+  function part() {
+    particles = [];
+    for (let i = 0; i < 50; i++) particles.push(new Particle());
+  }
+
+  // RESPOND TO USER SELECTION / INITIALIZATION
+  document.getElementById("balldrop").addEventListener("change", init);
+  function init() {
+    window.can
+    if (particles.length === 0) part();
+    dropping = true;
+    switch (document.getElementById("balldrop").value) {
+      case "no1": step = window.requestAnimationFrame(no1); break;
+      case "no2": step = window.requestAnimationFrame(no2); break;
+      case "no3": step = window.requestAnimationFrame(no3); break;
       default: break;
     }
+  }
+
+  // DOWNLOAD
+  document.getElementById("download").addEventListener("click", function () {
+    let type = "jpg";
+    let ele = document.createElement("a");
+    document.body.appendChild(ele);
+    ele.download = `mlorber_particles.${type}`;
+    ele.href = canvas.toDataURL(`image/${type};base64`);
+    ele.click();
+    ele.remove();
   });
 
-  
+  // PAUSE
+  document.getElementById("pause").addEventListener("click", pause);
+  function pause() {
+    if (dropping) dropping = false;
+    else {
+      dropping = true;
+      init();
+    }
+  }
 
-})();
+  // RELOAD
+  document.getElementById("reload").addEventListener("click", reload);
+  function reload() {
+    particles = [];
+    step = "";
+    ctx.clearRect(-100, -100, w + 100, h + 100);
+    window.cancelAnimationFrame(step);
+    init();
+  }
+
+  // STOP (MAKES THE PARTICLES FAIL TO BOUNCE OFF SIDES BY SPLICING THEM OUT OF THE ARRAY; WHEN ARRAY.LENGTH === 0, CALLS CANCELEVENTLISTENER)
+
+} main();
